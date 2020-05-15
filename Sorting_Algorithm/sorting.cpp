77ae0c474bb56c693fe7e5febeaafe6ffb60e4c2
Dennys Marcos <kardeenx@gmail.com>
2020-05-15 01:28:22 -0300

    okay working, now sleep

--- Sorting_Algorithm/sorting.cpp
+++ Sorting_Algorithm/sorting.cpp
@@ -6,36 +6,37 @@ void log(const char* message);
 int main(void)
 {
 	log("Welcome to this sorting algorithm.");
-	log("Choose up to 10 numbers so they will be returned in ascending order:");
-
-	int input;
-	std::cin >> input;
-
-	int length = std::to_string(input).length();
+	log("Choose 10 numbers (digits) so they will be returned in ascending order:");
 
 	int array[10];
+	int input;
 
-	for (int i = 5; i > 0; i--) {
-		array[i] = input % 10;
-		input /= 10;
-		std::cout << "Array[" << i <<"]: " <<array[i] << " - ";
+	for (int p = 1; p <= 10; p++)
+	{
+		std::cout << "Choose the " << p << " number:" << std::endl;
+		std::cin >> input;
+		array[p] = input;
+		
 	}
 
-	for (int j = 2; j = length; j++)
+	for (int j = 2; j <= 9; j++)
 	{
 		int key = array[j];
-		int i = j - 1;
-		while (i > 0 && array[i] > key)
+		int k = j - 1;
+		while (k > 0 && array[k] > key)
 		{
-			array[i + 1] = array[i];
-			i = i - 1;
+			array[k + 1] = array[k];
+			k = k - 1;
 		}
-		array[i + 1] = key;
+		array[k + 1] = key;
 	};
 
-
+	std::cout << std::endl;
 	log("Result after applying reordering algorithm:");
-	std::cout << array[10] << std::endl;
+	for (int l = 1; l <= 10; l++)
+	{
+		std::cout << array[l] << std::endl;
+	}
 	std::cin.get();
 }
 
